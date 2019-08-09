d24440219c38490f34551435b6503223d9f8466f
julius-verne <juliusvernedesign@gmail.com>
2019-08-09 18:20:24 -0500

    August

---
+++ august/script/load_all.R
@@ -0,0 +1,14 @@
+library(tidyverse)
+install.packages("tidyverse")
+
+survey.2017 <- read_csv(
+  "https://docs.google.com/spreadsheets/d/e/2PACX-1vThG-RH5yxJWbo-iAZ7s9xhSUwkeoEKTF7WMZT0yOXm8r3so8lm1R8FLZ7jvAo9eyOOUBBaoE3oQbnp/pub?gid=2115774317&single=true&output=csv"
+)
+
+survey.2018 <- read_csv(
+  "https://docs.google.com/spreadsheets/d/e/2PACX-1vSb0zzW9Hstc1FKD3ypS1uXZUcIXHZcHUFMBK_7AtMK7zYeL4oiUlhINGH5L_4SMfovTFzJMCMx_vsB/pub?gid=1926742553&single=true&output=csv"
+)
+
+survey.2019 <- read_csv(
+  "https://docs.google.com/spreadsheets/d/e/2PACX-1vQlKBlheOfocbBLn5JKndPsVPMJ5UCBgY5n884oJrgh4y4CCCMzYGzRiKGF_dBp9DOK-3-2_H8KYnH6/pub?gid=818679923&single=true&output=csv"
+)
