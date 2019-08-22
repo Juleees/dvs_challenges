e111c1a90210381a2295e6ddf111a5398b574144
julius-verne <juliusvernedesign@gmail.com>
2019-08-22 00:12:09 -0500

    Im Late

--- august/script/load_all.R
+++ august/script/load_all.R
@@ -1,5 +1,4 @@
 library(tidyverse)
-install.packages("tidyverse")
 
 survey.2017 <- read_csv(
   "https://docs.google.com/spreadsheets/d/e/2PACX-1vThG-RH5yxJWbo-iAZ7s9xhSUwkeoEKTF7WMZT0yOXm8r3so8lm1R8FLZ7jvAo9eyOOUBBaoE3oQbnp/pub?gid=2115774317&single=true&output=csv"
